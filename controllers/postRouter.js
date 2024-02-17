a97da9ad6dfb0b669c548c8495abd4191d0a0256
jeff1999git <jeffinmjtcr@gmail.com>
2024-02-17 20:27:43 +0530

    backend ready

---
+++ controllers/postRouter.js
@@ -0,0 +1,21 @@
+const express=require("express")
+
+const router=express.Router()
+
+const postModel=require("../models/postModel")
+
+router.post("/addpost",async(req,res)=>{
+    let data=req.body
+    let post=new postModel(data)
+    let result=await post.save()
+    res.json({
+        status:"success"
+    })
+})
+
+router.get("/viewpost",async(req,res)=>{
+    let result=await postModel.find()
+    res.json(result)
+})
+
+module.exports=router
--- controllers/signRouter.js
+++ controllers/signRouter.js
@@ -1,13 +1,54 @@
 const express=require("express")
 
 const router=express.Router()
+const userModel=require("../models/userModel")
 
-router.post("/post",(req,res)=>{
-    res.send("Post Working")
+router.post("/signup",async(req,res)=>{
+    let data=req.body
+    let userObj=new userModel(data)
+    let result=await userObj.save()
+    res.json({
+        status:"success"
+    })
 })
 
-router.get("/get",(req,res)=>{
-    res.send("Get Working")
+router.post("/signin",async(req,res)=>{
+    let input=req.body
+    let email=req.body.userEmail
+    let pword=req.body.userPword
+    let edata=await userModel.findOne({"userEmail":email})
+    let pdata=await userModel.findOne({"userPword":pword})
+    if(!edata)
+    {
+        return res.json(
+            {
+                status:"invalid email"
+            }
+            
+        )
+    }
+    if(!pdata)
+    {
+        return res.json(
+            {
+                status:"invalid password"
+            }
+            
+        )
+    }
+    res.json(
+        {
+            status:"success"
+        }
+    )
 })
+
+router.get("/viewusers", async (req, res) => {
+    let projection = "-_id -__v"; // Using string syntax to exclude fields
+    let result = await userModel.find({}, projection); // Apply projection in find
+    res.json(result);
+    
+});
+
 
 module.exports=router
