ab61cd844ccf4fdd9dd3702c5620a3be205a9044
jeff1999git <jeffinmjtcr@gmail.com>
2024-02-17 10:57:07 +0530

    route set

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express=require("express")
 const cors=require("cors")
 const mongoose=require("mongoose")
-
+const signRoute=require("./controllers/signRouter")
 const app=express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use("/api/sign",signRoute)
 
 app.listen(3001,()=>{
     console.log("Server Running....")
